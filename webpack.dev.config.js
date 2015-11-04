96162be171dbc74e86c4c06bbc7ececc6cb41e6f
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2015-11-04 19:49:09 +0000

    run .js files through Babel

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -19,7 +19,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.jsx$/,
+        test: /\.js/,
         loaders: ['react-hot', 'babel'],
         include: path.join(__dirname, 'src')
       }
