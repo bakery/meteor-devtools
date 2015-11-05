311be6536fce2bb5201bf6f55defb3859397cd13
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2015-11-05 00:24:02 +0000

    fix labels for messages

--- __tests__/processors.js
+++ __tests__/processors.js
@@ -5,15 +5,30 @@ const DDPGenerator = require('../src/ddp-generator');
 const TraceProcessor = require('../src/trace-processor');
 const Store = require('../src/store');
 
-describe('Message Processors', () => {
+let runProcessor = (traces, isOutbound=true) => {
+  // XX: need to prepare traces using
+  // Store.transforMessage before passing it to TraceProcessor
+  return TraceProcessor.processTraces(
+    _.map(traces, (m) => Store.transformMessage(m,isOutbound))
+  );
+};
 
+let testLabel = (messages, expectedLabel) => {
+  let actualLabel = runProcessor(
+    _.isArray(messages) ? messages : [messages]
+  )[0].label; 
+
+  expect(actualLabel).toEqual(expectedLabel);
+};
+
+describe('Message Processor', () => {
   it('groups messages that belong to the same group', () => {
     let numberOfMessages = 4;
     let batch1 = DDPGenerator.generate({
-      type : 'added', numberOfMessages : numberOfMessages
+      type : 'added', numberOfMessages
     });
     let batch2 = DDPGenerator.generate({
-      type : 'added', numberOfMessages : numberOfMessages
+      type : 'added', numberOfMessages
     });
     let isOutbound = false;
 
@@ -31,11 +46,84 @@ describe('Message Processors', () => {
     };
 
     _.each([batch1,batch2], (messages) => {
-      let processedTraces = TraceProcessor.processTraces(
-        _.map(messages, (m) => Store.transformMessage(m,isOutbound))
-      );
-
+      let processedTraces = runProcessor(messages, isOutbound);
       runTestsOnBatch(messages, processedTraces);
     });
+  });
+
+  it('attaches correct label for **added/removed/changed** messages', () => {
+    let numberOfMessages = 3;
+    let labelsForMessageTypes = {
+      added : {
+        singleItemLabel : (i) => {
+          return `item added to ${i.collection} collection`
+        },
+        multipleItemsLabel : (is) => {
+          return `${numberOfMessages} items added to ${is[0].collection} collection`  
+        }
+      },
+
+      removed : {
+        singleItemLabel : (i) => {
+          return `item removed from ${i.collection} collection`
+        },
+        multipleItemsLabel : (is) => {
+          return `${numberOfMessages} items removed from ${is[0].collection} collection`  
+        }
+      },
+
+      changed : {
+        singleItemLabel : (i) => {
+          return `item changed in ${i.collection} collection`
+        },
+        multipleItemsLabel : (is) => {
+          return `${numberOfMessages} items changed ${is[0].collection} collection`  
+        }
+      }
+    };
+
+    _.each(_.keys(labelsForMessageTypes), (type) => {
+      let message = DDPGenerator.generate({ type });
+      let messages = DDPGenerator.generate({type, numberOfMessages});
+      
+      testLabel(message, 
+        labelsForMessageTypes[type].singleItemLabel.call(this,message)
+      );
+      testLabel(messages, 
+        labelsForMessageTypes[type].multipleItemsLabel.call(this,messages)
+      ); 
+    });
+  });
+
+  it('attaches correct label for **ping/pong/connect/updated/result**', () => {
+    _.each(['ping','pong','connect','updated','result'], (type) => {
+      let message = DDPGenerator.generate({ type });
+      testLabel(message, type);
+    });
+  });
+
+  it('attaches correct label for **sub**', () => {
+    let message = DDPGenerator.generate({ type : 'sub' });
+    let params = message.params.join(', ')
+    testLabel(message, 
+      `subscribing to ${message.name} with ${params}`);
+  });
+
+  it('attaches correct label for **ready**', () => {
+    let message = DDPGenerator.generate({ type : 'ready' });
+    let subs = message.subs.join(', ')
+    testLabel(message, `subscription ready for ${subs}`);
+  });
+
+  it('attaches correct label for **method**', () => {
+    let message = DDPGenerator.generate({ type : 'method' });
+    let params = message.params.join(', ')
+    testLabel(message, 
+      `calling method ${message.method} with ${params}`);
+  });
+
+  it('attaches correct label for unknown messages', () => {
+    let randomMessageType = 'randommessagetype';
+    testLabel({msg: randomMessageType}, randomMessageType);
   });
 });
--- src/processors/labels.js
+++ src/processors/labels.js
@@ -1,33 +1,56 @@
 import _ from 'underscore';
 
+const _labels = {
+  added : (message) => {
+    return _.isArray(message) ?
+      `${message.length} items added to ${message[0].collection} collection` : 
+      `item added to ${message.collection} collection`;
+  },
+
+  removed : (message) => {
+    return _.isArray(message) ?
+      `${message.length} items removed from ${message[0].collection} collection` :
+      `item removed from ${message.collection} collection`;
+  },
+
+  changed : (message) => {
+    return _.isArray(message) ?
+      `${message.length} items changed ${message[0].collection} collection` :
+      `item changed in ${message.collection} collection`;
+  },
+
+  ping : () => 'ping',
+  pong : () => 'pong',
+  connect : () => 'connect',
+  updated : () => 'updated',
+  result : () => 'result',
+
+  sub : (message) => {
+    let params = (message.params || []).join(', ')
+    return `subscribing to ${message.name} with ${params}`;
+  },
+
+  ready : (message) => {
+    let subs = (message.subs || []).join(', ')
+    return `subscription ready for ${subs}`;
+  },
+
+  method : (message) => {
+    let params = (message.params || []).join(', ')
+    return `calling method ${message.method} with ${params}`;
+  }
+};
+
 class TraceLabels {
   run(traces) {
     return _.map(traces, (t) => {
-      let label = t.message.msg;
-
-      if(_.isArray(t.message)){
-        // dealing with a grouped message
-        const count = t.message.length;
-        const operation = _.first(t.message).msg;
-        const collection = _.first(t.message).collection;
-        label = operation;
-
-        switch(operation){
-          case 'added':
-            label = `${count} items added to ${collection} collection`;
-            break;
-          case 'removed':
-            label = `${count} items removed from ${collection} collection`;
-            break;
-          case 'changed':
-            label = `${count} items changed in ${collection} collection`;
-            break;
-        }
-      }
-
-      return _.extend(t,{
-        label : label
-      });
+      let messageType = _.isArray(t.message) ? 
+        t.message[0].msg : t.message.msg;
+      let label = _labels[messageType] ? 
+        _labels[messageType].call(this,t.message) :
+        messageType;
+      
+      return _.extend(t, { label });
     });
   }
 };
