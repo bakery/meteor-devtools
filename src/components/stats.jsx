f2956976506f19c7f7b0223bcc302bb56fea55d8
Mark Downey <mark@thebakery.io>
2016-03-02 14:35:43 +0000

    format stats

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -10,13 +10,17 @@ export default React.createClass({
   render (){
     return (
       <div>
-        <i className="fa fa-arrow-down"></i>&nbsp;
-        <strong>{this.props.stats.inboundMessages}</strong>&nbsp;
-        <i className="fa fa-arrow-up"></i>&nbsp;
-        <strong>{this.props.stats.outboundMessages}</strong>&nbsp;|&nbsp;
-        <strong>collection ops: {this.props.stats.messageTypes.collections}</strong>&nbsp;
-        <strong>method calls: {this.props.stats.messageTypes.methodCalls}</strong>&nbsp;
-        <strong>subs: {this.props.stats.messageTypes.subscriptions}</strong>
+        <span title="inbound messages">
+          <i className="fa fa-arrow-down"></i>&nbsp;
+          <strong>{this.props.stats.inboundMessages}</strong>&nbsp;
+        </span>
+        <span title="outbound messages">
+          <i className="fa fa-arrow-up"></i>&nbsp;
+          <strong>{this.props.stats.outboundMessages}</strong>
+        </span>&nbsp;|&nbsp;
+        <strong title="collection operations">collection ops: {this.props.stats.messageTypes.collections}</strong>&nbsp;|&nbsp;
+        <strong title="method calls">method calls: {this.props.stats.messageTypes.methodCalls}</strong>&nbsp;|&nbsp;
+        <strong title="subscriptions">subs: {this.props.stats.messageTypes.subscriptions}</strong>
       </div>
     )
   }
