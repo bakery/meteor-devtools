20d2fbc61dde1561d0098bcbcd7367eaf9990370
Mark Downey <mark@thebakery.io>
2016-04-04 13:57:17 +0200

    style tabs

--- src/plugins/blaze/index.jsx
+++ src/plugins/blaze/index.jsx
@@ -66,8 +66,8 @@ class App extends Component {
     return (
       <div className="blaze-inspector">
         <section>
-          <div style={{borderBottom:'solid 1px #ccc', marginBottom: '11px'}}>👷 Yo! This stuff here is pretty experimental. Let us know what you think and if it works ->
-          &nbsp;<a href="https://twitter.com/bakeryhq" target="_blank">Talk to us</a></div>
+          <header style={{borderBottom:'solid 1px #ccc', marginBottom: '11px'}}>👷 Yo! This stuff here is pretty experimental. Let us know what you think and if it works ->
+          &nbsp;<a href="https://twitter.com/bakeryhq" target="_blank">Talk to us</a></header>
           { rootNode ? 
             <BlazeTreeView rootNode={rootNode}
             getChildNodes={this.props.getChildNodes}
